ebf38aa6c9166d563166260c8d8f8a3093970e34
hongseekim <songhee0617@gmail.com>
2024-10-11 21:03:19 +0900

    Initial Commit

---
+++ MY-SOPT-Seminar/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MY-SOPT-Seminar
+//
+//  Created by 김송희 on 10/11/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
